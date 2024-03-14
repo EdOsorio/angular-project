cd2c46a1ac8e643a3d804b01e07c1c3480e92f53
EdOsorio <106685409+EdOsorio@users.noreply.github.com>
2024-03-14 18:21:39 -0500

    Creating web component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: []
+  // entryComponents: [AppComponent]
+})
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {
+    const webComponent = createCustomElement(AppComponent, {
+      injector: this.injector
+    });
+    customElements.define('angular-component', webComponent);
+  }
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'angular-project' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-project');
-  });
-});
+  ngDoBootstrap() {}
+}
