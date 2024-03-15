9408c4f141339915c6e4749924f176051e6d3d3b
EdOsorio <106685409+EdOsorio@users.noreply.github.com>
2024-03-14 22:42:54 -0500

    Deleted dist from gitignore

---
+++ dist/angular-project/server/chunk-SKJNXDI4.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as n,d as p,e as m,l as a,n as f,o as s,r as c,x as d}from"./chunk-MRH3I4EF.mjs";var l=(()=>{let o=class o{constructor(){this.title="angular-project"}};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,A){e&1&&(i(0,"div"),p(1,"Angular component"),n())}});let t=o;return t})();var u=[];var g={providers:[d(u),s()]};var x={providers:[c()]},v=a(g,x);var y=()=>f(l,v),q=y;export{q as a};
